749b5985164a664671f9bd8ae0951c4325128458
EzeTxle <60270488+EzeTxle@users.noreply.github.com>
2024-03-28 01:34:43 -0300

    v1.0.0

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using SML;
+using HarmonyLib;
+using Server.Shared.State;
+using Server.Shared.Info;
+using Art;
+using Services;
+using System.Reflection;
+using UnityEngine;
+using Home.Common.Settings;
+
+namespace KYS
+{
+    
+	[Mod.SalemMod]
+	class Main{
+		public static void Start(){
+			Debug.Log("Working?");
+		}
+	}
+    [HarmonyPatch(typeof(SettingsController), "Start")]
+	class Kill{
+		public static void Prefix(SettingsController __instance){
+			__instance.deleteAccountGO.SetActive(false);
+	}
+	}
+}
